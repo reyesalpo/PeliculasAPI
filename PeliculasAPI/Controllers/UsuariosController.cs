3cd74c9ec7b94bcf821519afad38c1a5b122ee79
reyesalpo <reyesmoreno606@gmail.com>
2026-05-15 18:06:10 -0600

    agregar el endpoint de crearUsuario

--- PeliculasAPI/Controllers/UsuariosController.cs
+++ PeliculasAPI/Controllers/UsuariosController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Migrations;
+using PeliculasAPI.Modelos;
 using PeliculasAPI.Modelos.Dtos;
 using PeliculasAPI.Repositorio.IRepositorio;
+using System.Net;
 
 namespace PeliculasAPI.Controllers
 {
@@ -12,11 +15,13 @@ namespace PeliculasAPI.Controllers
     {
         private readonly IUsuarioRepositorio _usuarioRepositorio;
         private readonly IMapper _mapper;
+        protected RespuestaAPI _respuestaApi;
 
         public UsuariosController(IUsuarioRepositorio usuarioRepositorio, IMapper mapper)
         {
             _usuarioRepositorio = usuarioRepositorio;
             _mapper = mapper;
+            this._respuestaApi = new();
         }
 
         [HttpGet]
@@ -34,6 +39,58 @@ namespace PeliculasAPI.Controllers
             }
 
             return Ok(listaUsuariosDto);
+        }
+
+        [HttpGet("{usuarioId:int}", Name = "GetUsuario")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUsuario(int usuarioId)
+        {
+            var usuario = _usuarioRepositorio.GetUsuario(usuarioId);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            var itemUsuarioDto = _mapper.Map<UsuarioDto>(usuario);
+
+            return Ok(itemUsuarioDto);
+        }
+
+        [HttpPost("Registro")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> AgregarUsuario([FromBody] UsuarioRegistroDto usuarioRegistroDto)
+        {
+            bool validarNombreUSuarioUnico = _usuarioRepositorio.IsUniQueUSer(usuarioRegistroDto.NombreUsuario);
+
+            if (!validarNombreUSuarioUnico)
+            {
+                _respuestaApi.StatusCode = HttpStatusCode.BadRequest;
+                _respuestaApi.IsSuccess = false;
+                _respuestaApi.ErrorMessages.Add("El nombre de usuario ya existe");
+                return BadRequest(_respuestaApi);
+            }
+
+            var usuario = await _usuarioRepositorio.Registro(usuarioRegistroDto);
+            if (usuario == null)
+            {
+                _respuestaApi.StatusCode = HttpStatusCode.BadRequest;
+                _respuestaApi.IsSuccess = false;
+                _respuestaApi.ErrorMessages.Add("Error en el registro");
+                return BadRequest(_respuestaApi);
+            }
+
+            _respuestaApi.StatusCode = HttpStatusCode.OK;
+            _respuestaApi.IsSuccess = true;
+
+
+            return Ok(_respuestaApi);
         }
     }
 }
---
+++ PeliculasAPI/Modelos/RespuestaAPI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OpenApi.MicrosoftExtensions;
+using System.Net;
+
+namespace PeliculasAPI.Modelos
+{
+    public class RespuestaAPI
+    {
+        public RespuestaAPI()
+        {
+            ErrorMessages = new List<string>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public Object Result { get; set; }
+    }
+}
